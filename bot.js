e746f67a604aa1908890552cdadfe1780e226b80
Playermode <36794669+Playermode@users.noreply.github.com>
2018-03-17 18:51:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -295,7 +295,7 @@ client.on("message", function(message) {
         break;
     case "otherinfo":
          var otherinfo1 = new Discord.RichEmbed()
-            .setDescription("8. Memes can contain swearing, **but** you can not swear in #memes")
+            .setDescription("9. Memes can contain swearing, **but** you can not swear in #memes")
          message.channel.sendEmbed(otherinfo1);
          break;
     case "imageinfo":  
