241ae5b430228f884c1d3344f6485efe2e901100
Playermode <36794669+Playermode@users.noreply.github.com>
2018-04-05 18:36:03 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -556,7 +556,7 @@ switch (args[0].toLocaleLowerCase()) {
 client.on('message', message => {
     if (message.content === "hi") {
     	message.reply('Hello!');
-  	}    
+  }}    
 });
       
 // THIS BOT IS ONLY FOR EGGTOWN
