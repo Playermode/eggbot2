ac467c823b37cb2aa3d75489731c7a0dd0fb2614
Playermode <36794669+Playermode@users.noreply.github.com>
2018-04-05 12:59:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -524,7 +524,7 @@ client.on("message", function(message) {
 
 client.on('ready', function() { 
     client.user.setGame("Eggtown Rewritten");
-    client.user.setUsername("Pengkip");
+    client.user.setUsername("Egg Bot");
     console.log('I am ready!');
 });
 
