43bb404ba17b1a9e9e1b55940b27c5dc5a8bd76b
Playermode <36794669+Playermode@users.noreply.github.com>
2018-04-08 12:36:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -523,7 +523,7 @@ client.on("message", function(message) {
 });
 
 client.on('ready', function() { 
-    client.user.setGame("BrantSteele Hunger Games simulator");
+    client.user.setGame("Listening to Frank Sinatra");
     client.user.setUsername("Egg Bot");
     console.log('I am ready!');
 });
