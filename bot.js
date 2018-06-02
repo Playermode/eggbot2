674b3bf1f0aca60cc729bed1de26ca7936f944b6
Playermode <36794669+Playermode@users.noreply.github.com>
2018-06-01 21:19:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -384,6 +384,7 @@ client.on("message", function(message) {
 }  
 
   if (message.content === "good") {
+    const goodegg = client.emojis.get("452278165134376971");  
     message.react("452278165134376971");
 }   
 
