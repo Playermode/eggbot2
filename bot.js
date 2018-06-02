80032631824ac3ba983395e9f1cab39d50d0d697
Playermode <36794669+Playermode@users.noreply.github.com>
2018-06-01 20:51:27 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -395,6 +395,12 @@ client.on("message", function(message) {
       if (args[0]) message.channel.sendMessage(guysciencedms[Math.floor(Math.random() * guysciencedms.length)])
       else message.channel.sendMessage("no xDDDDDD");
       break;
+     case "listemojis":
+    if (message.content === "listemojis") {
+    const emojiList = message.guild.emojis.map(e=>e.toString()).join(" ");
+    else message.channel.send(emojiList);     
+}        
+    break;         
     case "doit":
         var doit = new Discord.RichEmbed()
             .setImage("https://media2.giphy.com/media/3o84sw9CmwYpAnRRni/giphy.gif")
@@ -801,12 +807,6 @@ client.on('message', message => {
     if (message.content === "muffin") {
     	message.reply('ah nou');
     }    
-});
-
-if (message.content === "listemojis") {
-  const emojiList = message.guild.emojis.map(e=>e.toString()).join(" ");
-  message.channel.send(emojiList);
-    }
 });
 
 //THIS BOT IS ONLY FOR EGGTOWN
