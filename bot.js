466415f5928c69c565e9d2762e40fe405582a2e5
Playermode <36794669+Playermode@users.noreply.github.com>
2018-06-03 11:22:31 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -752,19 +752,18 @@ client.on("message", function(message) {
 });
     case "realnews":
          var realnews = new Discord.RichEmbed()
-             .setTitle("This is your title, it can hold 256 characters")
+             .setTitle("📰**News**")
              .setAuthor("Playermode", "https://i.imgur.com/qtewCuV.png")
              .setColor(0x00AE86)
-             .setDescription("This is the main body of text, it can hold 2048 characters.")
-             .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-             .setImage("http://i.imgur.com/yVpymuV.png")
-             .setThumbnail("http://i.imgur.com/p2qNFag.png")
+             .setFooter("© Eggtown", "http://i.imgur.com/w1vhFSR.png")
+             .setImage("https://i.imgur.com/9PQaqp3.png")
              .setTimestamp()
              .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-             .addField("This is a field title, it can hold 256 characters", "This is a field value, it can hold 2048 characters.")
-             .addField("Inline Field", "They can also be inline.", true)
-             .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
-         message.channel.send(realnews);
+             .addField("- Today is national eggday!", " ￼ ")
+             .addField("- There will be many eggy things occuring today", " ￼ ")
+             .addField("- Make sure to check #polls-and-giveaways for giveaways today (do !pollsrole to be notified when a new poll or giveaway comes out)", " ￼ ")
+             .addField("- Have an eggy day!, " ￼ ");
+         message.channel.sendEmbed(realnews);
          break;        
   }
 });
