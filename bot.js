ca822ff741d996b778b93d9462e34b8422f9494c
Playermode <36794669+Playermode@users.noreply.github.com>
2018-06-03 02:36:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -688,11 +688,35 @@ client.on("message", function(message) {
           message.channel.sendEmbed(news2);
           break;
     case "test1":
-          var test1 = new Discord.RichEmbed()
-              .addField("egg", " ￼ ")
-              .setVideo("https://youtu.be/rf_Go9eLaAM")                
-          message.channel.sendEmbed(test1);
-          break;          
+         message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});
   }
 });
 
