72883dcc6e56aa6b1029b8152c3b57c7267cc0af
Playermode <36794669+Playermode@users.noreply.github.com>
2018-07-13 18:49:27 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -926,8 +926,8 @@ client.on("message", function(message) {
          break;  
     case "defcon":
          var defcon = new Discord.RichEmbed() 
-             .setTitle("Eggtown 2 is currently in **defcon 3**")
-             .setImage("https://cdn.discordapp.com/attachments/451836383523242007/467055139992240140/defcon3.png")
+             .setTitle("Eggtown 2 is currently in **defcon 4**")
+             .setImage("https://cdn.discordapp.com/attachments/451836383523242007/467055138150940704/defcon4.png")
          message.channel.sendEmbed(defcon);
          break;    
   }
